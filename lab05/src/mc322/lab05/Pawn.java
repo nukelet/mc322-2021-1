f094df87933f974ed1b024f117e24c2f9cfe9551
Vinícius Peixoto <vinicius241201@gmail.com>
2021-04-22 16:57:52 -0300

    Add classes Position, Pawn and Color

---
+++ lab05/src/mc322/lab05/Pawn.java
@@ -0,0 +1,32 @@
+package mc322.lab05;
+
+import java.lang.Math;
+
+class Pawn {
+    Position position;
+    Color color;
+
+    Pawn(Position position, Color color) {
+        this.position = position;
+        this.color = color;
+    }
+
+    public boolean isValidMove(Position pos) {
+        int i1 = position.getX(), j1 = position.getY();
+        int i2 = pos.getX(), j2 = pos.getY();
+        
+        if (Math.abs(i1 - i2) == 1 && Math.abs(j1 - j2) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setPosition(Position position) {
+        if (isValidMove(position)) {
+            this.position = position;
+        } else {
+            System.err.println("Invalid move: " + position.toString());
+        }
+    }
+}
