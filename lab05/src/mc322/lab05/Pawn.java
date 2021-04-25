50b6862d17e91c2dd6c51a84415c8e3d856a5a35
Vinícius Peixoto <vinicius241201@gmail.com>
2021-04-25 13:22:15 -0300

    [lab05] Add isValidMove() implementation for Pawn in lab05

--- lab05/src/mc322/lab05/Pawn.java
+++ lab05/src/mc322/lab05/Pawn.java
@@ -11,11 +11,30 @@ class Pawn {
         this.color = color;
     }
 
-    public boolean isValidMove(Position pos) {
-        int i1 = position.getX(), j1 = position.getY();
-        int i2 = pos.getX(), j2 = pos.getY();
+    public boolean isValidMove(Position destination) {
+        int i1 = position.getI(), j1 = position.getJ();
+        int i2 = destination.getI(), j2 = destination.getJ();
+
+        // indicates wheter a piece moves upwards or downwards
+        // on the board when it's not capturing another piece
+        int offset;
+        if (getColor() == Color.WHITE) {
+            // white pieces always go from a->h (0->7)
+            offset = 1;
+        } else {
+            // black pieces always go from h->a (7->0)
+            offset = -1;
+        }
         
+        // when moving without capturing a piece
         if (Math.abs(i1 - i2) == 1 && Math.abs(j1 - j2) == 1) {
+            if (i2 == i1 + offset) {
+                return true;
+            } else {
+                return false;
+            }
+        } // when capturing a piece
+        else if (Math.abs(i1 - i2) == 2 && Math.abs(j1 - j2) == 2) {
             return true;
         } else {
             return false;
@@ -25,7 +44,9 @@ class Pawn {
     public Position getPosition() {
     	return position;
     }
-
+    
+    // TODO: I think this isValidMove() check should be called from the
+    // Board class, not from here
     public void setPosition(Position position) {
         if (isValidMove(position)) {
             this.position = position;
