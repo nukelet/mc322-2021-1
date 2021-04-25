fc30153c79f55b13b4aa1576eacb302a82ca5a6f
Vinícius Peixoto <vinicius241201@gmail.com>
2021-04-25 13:24:12 -0300

    [lab05] Add isValidMove() implementation for Queen

--- lab05/src/mc322/lab05/Queen.java
+++ lab05/src/mc322/lab05/Queen.java
@@ -21,10 +21,16 @@ public class Queen {
 		return color;
 	}
 	
-	public boolean isValidMove(Piece[] piecesUntilTarget) {
-		// TODO: implement method
-		return false;
-	}
+    public boolean isValidMove(Position destination) {
+        int i1 = position.getI(), j1 = position.getJ();
+        int i2 = destination.getI(), j2 = destination.getJ();
+
+        if (Math.abs(i1 - i2) == Math.abs(j1-j2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }	
 	
 	@Override
 	public String toString() {
