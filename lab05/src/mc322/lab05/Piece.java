06d4f5180403ff411589ea822f98c791f44e4777
Vinícius Peixoto <vinicius241201@gmail.com>
2021-04-30 19:44:35 -0300

    [lab05] Refactor Piece into an abstract class

--- lab05/src/mc322/lab05/Piece.java
+++ lab05/src/mc322/lab05/Piece.java
@@ -1,97 +1,34 @@
 package mc322.lab05;
 
-public class Piece {
+public abstract class Piece {
 	enum PieceType { PAWN, QUEEN };
-	
-	PieceType type;
-	Pawn pawn;
-	Queen queen;
-	
+    
+    protected Position position;
+    protected Color color;
+
 	public Piece(Position position, Color color) {
-		this.type = PieceType.PAWN;
-		this.pawn = new Pawn(position, color);
-		this.queen = null;
+        this.position = position;
+        this.color = color;
 	}
-	
-	public PieceType getType() {
-		return type;
-	}
-	
-	public Pawn getPawn() {
-		return pawn;
-	}
-	
-	public Queen getQueen() {
-		return queen;
-	}
-	
+			
 	public Position getPosition() {
-    	switch (type) {
-            case PAWN: 
-                return pawn.getPosition();
-            case QUEEN:
-                return queen.getPosition();
-    	}
-    	return null;
+        return this.position;
     }
 
     public void setPosition(Position position) {
-        switch (type) {
-            case PAWN:
-                pawn.setPosition(position);
-                // pawn promotion
-                if (getColor() == Color.WHITE && position.getI() == 7) {
-                    type = PieceType.QUEEN;
-                    pawn = null;
-                    queen = new Queen(position, Color.WHITE);
-                } else if (getColor() == Color.BLACK && position.getI() == 0) {
-                    type = PieceType.QUEEN;
-                    pawn = null;
-                    queen = new Queen(position, Color.BLACK);
-                }
-                break;
-            case QUEEN:
-                queen.setPosition(position);
-                break;
-        }
+        this.position = position;
     }
 	
 	public Color getColor() {
-		switch (type) {
-            case PAWN:
-                return pawn.getColor();
-            case QUEEN:
-                return queen.getColor();
-		}
-		return null;
+        return this.color;
 	}
 
-    public boolean isPawn() {
-        return type == PieceType.PAWN;
-    }
-
-    public boolean isQueen() {
-        return type == PieceType.QUEEN;
-    }
-
     public boolean isValidMove(Position destination) {
-        switch (type) {
-            case PAWN:
-                return pawn.isValidMove(destination);
-            case QUEEN:
-                return queen.isValidMove(destination);
-        }
         return false;
     }
 	
 	@Override
 	public String toString() {
-		switch (type) {
-		case PAWN:
-			return pawn.toString();
-		case QUEEN:
-			return queen.toString();
-		}
-		return "";
+        return null;
 	}
 }
